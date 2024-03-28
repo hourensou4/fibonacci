7485915d97900ec3c0956e43b6608b21ec06f513
“hourensou” <162790565+hourensou4@users.noreply.github.com>
2024-03-28 13:06:16 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 function fib(n){
   if(n === 0){
     return 0;
@@ -10,5 +10,22 @@ function fib(n){
 }
 const length = 40;
 for (let i = 0; i <= length; i++){
+  console.log(fib(i));
+}
+*/
+'use strict';
+const memo = new Map(); //一度計算した結果を保存しておく方法「メモ化」
+memo.set(0, 0);
+memo.set(1, 1);
+function fib(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
